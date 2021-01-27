a2bf7872543b811a2939e8bb9c04c5f1bc17ce86
togarin <iibaryshev@gmail.com>
2021-01-27 13:44:57 +0700

    start v0.2

--- src/Components/Persons.js
+++ src/Components/Persons.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import {
+  Button,
   Table,
   TableBody,
   TableCell,
@@ -11,15 +12,7 @@ import {
   Paper,
   IconButton,
 } from "@material-ui/core";
-// import { FlightTakeoffOutlinedIcon, Delete } from "@material-ui/icons";
 import FlightTakeoffOutlinedIcon from "@material-ui/icons/FlightTakeoffOutlined";
-// import Table from "@material-ui/core/Table";
-// import TableBody from "@material-ui/core/TableBody";
-// import TableCell from "@material-ui/core/TableCell";
-// import TableContainer from "@material-ui/core/TableContainer";
-// import TableHead from "@material-ui/core/TableHead";
-// import TableRow from "@material-ui/core/TableRow";
-// import Paper from "@material-ui/core/Paper";
 import InformationModal from "../Components/InformationModal";
 import axios from "axios";
 
@@ -28,39 +21,29 @@ const useStyles = makeStyles({
     minWidth: 650,
   },
 });
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-// const rows = [
-//   createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-//   createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-//   createData("Eclair", 262, 16.0, 24, 6.0),
-//   createData("Cupcake", 305, 3.7, 67, 4.3),
-//   createData("Gingerbread", 356, 16.0, 49, 3.9),
-// ];
 
 const DenseTable = () => {
   const classes = useStyles();
+  //   const [isFetching, setIsFetching] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const [persons, setPersons] = useState([]);
   const [informationModalOpen, setInformationModalOpen] = useState(false);
+  const [count, setCount] = useState(1);
 
-  const apiUrl = "https://swapi.dev/api/people/?page=1";
+  const apiUrl = `https://swapi.dev/api/people/?page=`;
 
   useEffect(() => {
     const fetchPersons = async () => {
       try {
-        const result = await axios.get(apiUrl);
-        console.log(result.data.results);
-
-        setPersons(result.data.results);
+        const response = await axios.get(apiUrl + `${count}`);
+        const newHeros = response.data.results;
+        setPersons((persons) => [...persons, ...newHeros]);
       } catch (error) {
         catchMessages(error);
       }
     };
     fetchPersons();
-  }, []);
+  }, [count]);
 
   const catchMessages = (error) => {
     if (error.response && error.response.status) {
@@ -111,6 +94,9 @@ const DenseTable = () => {
           </TableBody>
         </Table>
       </TableContainer>
+      <Button variant="outlined" onClick={() => setCount(count + 1)}>
+        More heros
+      </Button>
       <InformationModal
         isOpen={informationModalOpen}
         onClose={() => {
